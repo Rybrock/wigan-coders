ffd382e2663d55c8bf76d06477fccd07b9d60063
Rybrock <Ry.brock@ymail.com>
2022-11-16 19:51:03 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <app-navbar/>
+  <router-view/>
+</template>
+
+<script>
+import AppNavbar from './components/AppNavbar.vue';
+export default {
+  components: {
+    AppNavbar
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav class="p-5">
+      <div class="nav-wrapper flex place-items-center justify-start space-x-6">
+        <div class="px-0 md:px-5">
+          <span class="text-4xl font-semibold"><span class="text-red-500">&lt; </span>wigan coders<span class="text-red-500"> /></span></span>
+        </div>
+        <div class="font-bold space-x-8 hidden md:flex">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <font-awesome-icon icon="fa-solid fa-bars" />
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
