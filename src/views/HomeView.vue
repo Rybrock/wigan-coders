474198bf35d3766714eeb28905c385885c6dca80
Rybrock <Ry.brock@ymail.com>
2022-11-16 19:51:21 +0000

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
       <div class="text-left mt-4 w-full lg:w-7/12">
       <span class="font-bold text-3xl">LEARN TO CODE WITH US</span>
       <p class="w-full">Come to our meet-up event at Wigan Library every Saturday morning. Where we discuss and learn about all things coding.
-        We build exciting real world projects with HTML, CSS and JAVASCRIPT.
+        We build exciting real world projects with HTML, CSS and JAVASCRIPT
       </p>
     </div>
     <div class="w-full lg:w-5/12">
