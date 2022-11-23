0b333965d5939e252612f3ad81db061810426fad
Rybrock <Ry.brock@ymail.com>
2022-11-23 00:51:07 +0000

    complete for now

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
         <footer>
-            <div class="flex flex-col md:flex-row justify-between p-8 space-y-4 md:space-y-0">
+            <div class="flex flex-col md:flex-row justify-between p-8 space-y-6 md:space-y-0">
                 <div class="px-5 text-lg flex flex-col md:flex-row md:space-x-4">
                     <router-link to="/about">
                         <span>About</span>
@@ -11,11 +11,19 @@
                 </div>
                 <div>
                     <span>Find us on Social</span>
-                    <div class="flex items-center justify-center text-lg md:space-x-2 mt-4">
-                    <span><img src="../assets/facebook.png" alt="facebook footer icon" width="30" height="30"></span>
-                    <span><img src="../assets/instagram.png" alt="instagram footer icon" width="30" height="30"></span>
-                    <span><img src="../assets/twitter.png" alt="twitter footer icon" width="30" height="30"></span>
-                    <span><img src="../assets/github.png" alt="github footer icon" width="30" height="30"></span>
+                    <div class="flex items-center justify-center text-lg space-x-2 mt-4">
+                        <a href="https://www.facebook.com/profile.php?id=100088226045193" target="_blank">
+                            <span><img src="../assets/facebook.png" alt="facebook footer icon" width="30" height="30"></span>
+                        </a>
+                        <a href="https://instagram.com/wigan.coders?igshid=YmMyMTA2M2Y=" target="_blank">
+                            <span><img src="../assets/instagram.png" alt="instagram footer icon" width="30" height="30"></span>
+                        </a>
+                        <a href="https://twitter.com/coders_wigan" target="_blank">
+                            <span><img src="../assets/twitter.png" alt="twitter footer icon" width="30" height="30"></span>
+                        </a>
+                    <a href="https://github.com/Wigan-Coders-Git" target="_blank">
+                        <span><img src="../assets/github.png" alt="github footer icon" width="30" height="30"></span>
+                    </a>
                 </div>
                 </div>
                 <div class="px-5">
