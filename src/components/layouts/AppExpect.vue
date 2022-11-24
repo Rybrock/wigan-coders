ebffb67a90e3e8fb4f66143b994a0c16a2c4d5c0
Rybrock <Ry.brock@ymail.com>
2022-11-24 20:07:23 +0000

    complete for now

--- src/components/layouts/AppExpect.vue
+++ src/components/layouts/AppExpect.vue
@@ -1,36 +1,34 @@
 <template>
   <div class="my-24">
-    <div class="mb-14">
+    <div class="mb-5">
         <h2 class="text-4xl font-bold">Sound good?</h2>
     <p>Great, all you need is a laptop and heaps of enthusiasm!</p>
     </div>
-    <div class="flex flex-col items-center md:flex-row justify-between px-2">
-        <div class="w-full md:w-6/12">
-    <h2 class="text-2xl font-bold">What you'll need</h2>
-    <p>To get started with us, all you'll need is a laptop. Windows or Mac.</p>
+    <div class="flex flex-col items-center md:flex-row justify-center w-full">
+        <div class="w-full md:w-8/12">
+    <!-- <h2 class="text-2xl font-bold">What you'll need</h2>
+    <p>To get started with us, all you'll need is a laptop. Windows or Mac.</p> -->
     <div class="flex justify-center items-center flex-col w-full">
-        <div class="flex flex-col items-center md:flex-row justify-center space-x-10 mt-12 w-full">
-            <p class="w-full md:w-6/12 ">Preferably have VS Code installed on your laptop. This is a Text Editor, or IDE, and we use this to write our code. You can install it <a href="https://code.visualstudio.com/" class="font-bold" target="_blank">here</a></p>
+        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 md:w-8/12">
             <img src="../../assets/visual-studio-code-icon.png" width="70" height="40" alt="">
+            <p class="w-full">Preferably have VS Code installed on your laptop. This is a Text Editor, or IDE, and we use this to write our code. You can install it <a href="https://code.visualstudio.com/" class="font-bold" target="_blank">here</a></p>
         </div>
-        <div class="flex flex-col items-center md:flex-row justify-center space-x-10 mt-12 w-full">
-            <p class="w-full md:w-6/12 ">An account with GitHub. We can however do this on the day. We use GitHub to store our code and more. You can signup <a href="https://github.com/" class="font-bold" target="_blank">here</a></p>
+        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 md:w-8/12">
             <img src="../../assets/github.png" width="70" height="30" alt="">
+            <p class="w-full">An account with GitHub. We can however do this on the day. We use GitHub to store our code and more. You can signup <a href="https://github.com/" class="font-bold" target="_blank">here</a></p>
         </div>
-        <div class="flex flex-col items-center md:flex-row justify-center space-x-10 mt-12 w-full">
-            <p class="w-full md:w-6/12 ">For Mac users the Terminal app will already be installed. For Windows we use Git Bash. You'll find the link to install <a href="https://git-scm.com/downloads" class="font-bold" target="_blank">here</a></p>
+        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 md:w-8/12">
             <img src="../../assets/285695_terminal_icon.png" width="70" height="30" alt="">
+            <p class="w-full">For Mac users the Terminal app will already be installed. For Windows we use Git Bash. You'll find the link to install <a href="https://git-scm.com/downloads" class="font-bold" target="_blank">here</a></p>
         </div>
     </div>
+    <div class="mt-16">
+            <p class="text-2xl">register your interest with us <router-link to="/contact" class="font-bold">here</router-link></p>
         </div>
-        <div class="w-full md:w-6/12">
-    <h2 class="text-2xl font-bold">What to expect</h2>
-    <p>Great, all you need is a laptop and heaps of enthusiasm!</p>
+        </div>
         </div>
     </div>
-
-  </div>
-  <div class="flex flex-col w-full justify-center items-center p-2 mb-16 md:p-0">
+  <div class="flex flex-col w-full justify-center items-center p-2 mb-24 md:p-0">
     <p class="font-bold text-sm w-full lg:w-8/12">We are a none for profit organization with the view to helping people understand what coding is and how they go about beginning to learn. We offer free impartial advice and have a HTML, CSS and JAVASCRIPT curriculum coming soon!</p>
     <p class="flex font-bold w-full justify-center p-2 md:p-0 mt-4">*** +16 years and above</p>
 </div>
