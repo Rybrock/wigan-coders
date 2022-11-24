f8dfafc3cd7016c7d3dd89e3182655f266adf41c
Rybrock <Ry.brock@ymail.com>
2022-11-24 20:16:46 +0000

    last

--- src/components/layouts/AppExpect.vue
+++ src/components/layouts/AppExpect.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="my-24">
+  <div class="my-24 p-4 md:p-0">
     <div class="mb-5">
         <h2 class="text-4xl font-bold">Sound good?</h2>
     <p>Great, all you need is a laptop and heaps of enthusiasm!</p>
@@ -9,15 +9,15 @@
     <!-- <h2 class="text-2xl font-bold">What you'll need</h2>
     <p>To get started with us, all you'll need is a laptop. Windows or Mac.</p> -->
     <div class="flex justify-center items-center flex-col w-full">
-        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 md:w-8/12">
+        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 space-y-4 nd:space-y-0 md:w-8/12">
             <img src="../../assets/visual-studio-code-icon.png" width="70" height="40" alt="">
             <p class="w-full">Preferably have VS Code installed on your laptop. This is a Text Editor, or IDE, and we use this to write our code. You can install it <a href="https://code.visualstudio.com/" class="font-bold" target="_blank">here</a></p>
         </div>
-        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 md:w-8/12">
+        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 space-y-4 nd:space-y-0 md:w-8/12">
             <img src="../../assets/github.png" width="70" height="30" alt="">
             <p class="w-full">An account with GitHub. We can however do this on the day. We use GitHub to store our code and more. You can signup <a href="https://github.com/" class="font-bold" target="_blank">here</a></p>
         </div>
-        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 md:w-8/12">
+        <div class="flex flex-col items-center md:flex-row justify-center mt-12 w-full md:space-x-12 space-y-4 nd:space-y-0 md:w-8/12">
             <img src="../../assets/285695_terminal_icon.png" width="70" height="30" alt="">
             <p class="w-full">For Mac users the Terminal app will already be installed. For Windows we use Git Bash. You'll find the link to install <a href="https://git-scm.com/downloads" class="font-bold" target="_blank">here</a></p>
         </div>
